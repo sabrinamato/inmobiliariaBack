92fc25487135c63169292872fd26ce1400238149
sabirnamato <sabir.mato00@gmail.com>
2023-02-16 16:10:49 -0300

    register+login

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,28 @@
-const knex = require("../config/knexfile");
+const knex = require("../config/knex");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const TOKEN_SECRET = "ClaveToken";
+
+exports.registerUser = async (req, res) => {
+  const salt = await bcrypt.genSalt(10);
+  const contraseña = await bcrypt.hash(req.body.contraseña, salt);
+  const newUser = {
+    usuario: req.body.usuario,
+    email: req.body.email,
+    contraseña: contraseña,
+  };
+  knex("users")
+    .insert(newUser)
+    .then(() => {
+      res.status(200).json({ success: true, newUser: newUser });
+    })
+    .catch((error) => {
+      res.status(400).json({ error: message.error });
+    });
+};
 
 exports.login = async (req, res) => {
   const { email, contraseña } = req.body;
-  console.log(contraseña);
   await knex("users")
     .where("email", email)
     .then(async (usuarioDB) => {
@@ -18,9 +37,8 @@ exports.login = async (req, res) => {
       }
       const token = jwt.sign(
         {
-          nombre: usuario.nombre,
+          usuario: usuario.usuario,
           id: usuario.id,
-          tipousuario: usuario.tipousuario,
         },
         TOKEN_SECRET
       );
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { login } = require("../controllers/auth");
+const { registerUser, login } = require("../controllers/users");
 
+router.post("/register", registerUser);
 router.post("/login", login);
 
 module.exports = router;
